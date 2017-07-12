ad319203ca0bf742e57360a18805076035913800
cerezafuwa <cerezafuwa@gmail.com>
2017-07-12 15:36:30 +0800

    init resume

---
+++ my-resume/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <v-header></v-header>
+  </div>
+</template>
+
+<script>
+import header from './components/header'
+export default {
+  name: 'app',
+  components: {
+    'v-header': header
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ my-resume/src/components/header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="header">
+    {{ msg }}
+  </div>
+</template>
+<script>
+
+  export default{
+    name: 'header',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
+</script>
+<style scoped>
+  #header{
+    height: 400px;
+  }
+</style>
