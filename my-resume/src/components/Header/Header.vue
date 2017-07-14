395d3dbadb03c75c90a7a248aaf19f0af0a7c6f7
hubeini <hubeini@wondersgroup.com>
2017-07-14 10:22:31 +0800

    人生第一个vue！

---
+++ my-resume/src/components/Header/Header.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="header">
+    <div class="header-content">
+      <span>{{ project }}</span>
+      <span>{{ education }}</span>
+    </div>
+  </div>
+
+</template>
+<script type="text/ecmascript-6">
+  export default {
+    name: 'header',
+    data() {
+      return {
+        project: 'Project',
+        education: 'Education'
+      }
+    }
+  }
+</script>
+
+<style>
+  #header{
+    height: 40px;
+  }
+
+  .header-content{
+    display: flex;
+    justify-content:flex-end;
+  }
+
+  span{
+    margin: 7px;
+  }
+</style>
