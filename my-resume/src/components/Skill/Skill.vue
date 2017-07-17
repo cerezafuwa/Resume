6d73eb5a7f87729880f024245312df41bead8629
hubeini <hubeini@wondersgroup.com>
2017-07-17 10:36:42 +0800

    skill

---
+++ my-resume/src/components/Skill/Skill.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="skill">
+    <div class="skill-bg">
+
+    </div>
+  </div>
+</template>
+<script type="text/ecmascript-6">
+  export default{
+    name: 'skill'
+  }
+</script>
+<style>
+  #skill{
+    margin-top: 150px;
+  }
+  .skill-bg{
+    height: 150px;
+  }
+</style>
