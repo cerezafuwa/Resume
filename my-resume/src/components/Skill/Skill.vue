a5ccadc559e0eee8f013515b02e0cbf6b2ab54d8
hubeini <hubeini@wondersgroup.com>
2017-07-17 15:17:11 +0800

    本地调取json的问题，希望可以解决一下

--- my-resume/src/components/Skill/Skill.vue
+++ my-resume/src/components/Skill/Skill.vue
@@ -1,13 +1,65 @@
 <template>
   <div id="skill">
     <div class="skill-bg">
+      <ul class="skills">
+        <li v-for="skill in skills">
+          <p>{{skill.name.toUpperCase()}}</p>
+        </li>
 
+      </ul>
     </div>
   </div>
 </template>
+
 <script type="text/ecmascript-6">
   export default{
-    name: 'skill'
+    name: 'skill',
+    data(){
+        return{
+          "skills": [
+            {
+              "name": "javascript",
+              "score": 0.7
+            },
+            {
+              "name": "html",
+              "score": 0.9
+            },
+            {
+              "name": "css",
+              "score": 0.8
+            },
+            {
+              "name": "java",
+              "score": 0.75
+            },
+            {
+              "name": "c++",
+              "score": 0.7
+            },
+            {
+              "name": "c",
+              "score": 0.6
+            },
+            {
+              "name": "git",
+              "score": 1
+            },
+            {
+              "name": "photoshop",
+              "score": 0.95
+            },
+            {
+              "name": "illustrator",
+              "score": 0.95
+            },
+            {
+              "name": "corel painter",
+              "score": 0.75
+            }
+          ]
+        }
+    }
   }
 </script>
 <style>
@@ -16,5 +68,12 @@
   }
   .skill-bg{
     height: 150px;
+    margin-left: 100px;
+  }
+  .skills{
+    column-count: 2;
+  }
+  li{
+    display: inline;
   }
 </style>
