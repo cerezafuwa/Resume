7cce1cd087a8a411d183b500bc3db0161c8fcb21
hubeini <hubeini@wondersgroup.com>
2017-07-17 10:28:07 +0800

    bg完成!

--- my-resume/src/components/Introduction/Introduction.vue
+++ my-resume/src/components/Introduction/Introduction.vue
@@ -8,6 +8,11 @@
       <div class="introduction-rec5"></div>
       <div class="introduction-rec6"></div>
       <div class="introduction-rec7"></div>
+      <div class="introduction-rec8"></div>
+      <div class="introduction-rec9"></div>
+      <div class="introduction-rec10"></div>
+      <div class="introduction-rec11"></div>
+
     </div>
   </div>
 </template>
@@ -22,6 +27,8 @@ export default{
   .introduction-bg{
     width: 500px;
     height: 400px;
+    margin-left: 50px;
+    margin-right: 50px;
   }
   .introduction-rec1{
     position: relative;
@@ -45,13 +52,13 @@ export default{
   }
   .introduction-rec3{
     position: relative;
-    height: 50px;
-    width: 50px;
+    height: 60px;
+    width: 60px;
     background-color: #ffd442;
     border-radius: 10px;
     opacity: 0.5;
     left: 220px;
-    top: -130px;
+    top: -140px;
   }
   .introduction-rec4{
     position: relative;
@@ -95,12 +102,52 @@ export default{
   }
   .introduction-rec7{
     position: relative;
-    height: 70px;
-    width: 70px;
+    height: 50px;
+    width: 50px;
     background-color: lightpink;
     border-radius: 10px;
-    opacity: 0.8;
-    left:147px;
-    top:100px;
+    opacity: 0.5;
+    left:50px;
+    top:-40px;
+  }
+  .introduction-rec8{
+    position: relative;
+    height: 120px;
+    width: 120px;
+    background-color: #7cff76;
+    border-radius: 10px;
+    opacity: 0.5;
+    left:260px;
+    top:-190px;
+  }
+  .introduction-rec9{
+    position: relative;
+    height: 30px;
+    width: 30px;
+    background-color: #181eff;
+    border-radius: 5px;
+    opacity: 0.3;
+    left: 40px;
+    top:-300px;
+  }
+  .introduction-rec10{
+    position: relative;
+    height: 30px;
+    width: 30px;
+    background-color: #ffd442;
+    border-radius: 5px;
+    opacity: 0.5;
+    left: 210px;
+    top: -200px;
+  }
+  .introduction-rec11{
+    position: relative;
+    height: 30px;
+    width: 30px;
+    background-color: lightpink;
+    border-radius: 5px;
+    opacity: 0.7;
+    left:300px;
+    top:-180px;
   }
 </style>
